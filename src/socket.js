a9e1c3fa9ca58af6e070f48e6265fce3a4b35cf5
ahmedemad20031d <ahmed.emad.soliman.me@gmail.com>
2026-06-02 21:33:33 +0200

    sokect

--- src/socket.js
+++ src/socket.js
@@ -1,12 +1,11 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL =
-  "https://social-media-platform-production-42b8.up.railway.app";
-
-const socket = io(SOCKET_URL, {
-  transports: ["websocket", "polling"],
-  withCredentials: true,
-  forceNew: true,
-});
+const socket = io(
+  "https://social-media-platform-production-42b8.up.railway.app",
+  {
+    transports: ["websocket"],
+    withCredentials: true,
+  },
+);
 
 export default socket;
